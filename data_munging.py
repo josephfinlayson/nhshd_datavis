afd320effdab2cd6f6b91fccdf16eaaaad81cf43
Philip Ashton <flashton@Philips-MacBook-Air.local>
2014-05-24 21:46:32 +0100

    push for alex

---
+++ data_munging.py
@@ -0,0 +1,76 @@
+counties = ['ENGLAND', 'Avon', 'Barking_Havering', 'Barnet', 'Barnsley', 'Bedfordshire', 'Berkshire', 'Bexley_Greenwich', 'Birmingham', 'Bradford', 'Brent_Harrow', 'Bromley', 'Buckinghamshire', 'Bury_Rochdale', 'Cambridgeshire', 'Camden_Islington', 'Cornwall_IsleOfScilly', 'CountyDurham_Darlington', 'Coventry', 'Croydon', 'Doncaster', 'Dorset', 'Dudley', 'Ealing,Hammersmith_Hounslow', 'East_NorthHertfordshire', 'EastKent', 'EastLancashire', 'EastLondon_TheCity', 'EastRidingandHull', 'EastSussex', 'EASTERN', 'EasternSurrey', 'Enfield_Haringey', 'Gloucestershire', 'Herefordshire', 'Hillingdon', 'IsleOfWight', 'Kensington,Chelsea_Westminster', 'Kingston_Richmond', 'Lambeth,Southwark_Lewisham', 'Leeds', 'Leicestershire', 'Lincolnshire', 'Liverpool', 'LONDON', 'Manchester', 'Merton,Sutton_Wandsworth', 'MorceambeBay', 'NewcastleandNorthTyne', 'Norfolk', 'North_EastDevon', 'North_MidHampshire', 'NorthCheshire', 'NorthCumbria', 'NorthDerbyshire', 'NorthEssex', 'NorthNottinghamshire', 'NorthStaffordshire', 'NORTHWEST', 'NorthWestLancashire', 'NorthYorkshire', 'Northamptonshire', 'NORTHERN_YORKSHIRE', 'Northumberland', 'Nottingham', 'Oxfordshire', 'Portsmouth_SouthEastHampshire', 'Redbridge_WalthamForest', 'Rotherham', 'Salford_Trafford', 'Sandwell', 'Sefton', 'Sheffield', 'Shropshire', 'Solihull', 'Somerset', 'South_WestDevon', 'SouthCheshire', 'SOUTHEAST', 'SouthEssex', 'SouthHumber', 'SouthLancashire', 'SouthofTyne', 'SouthStaffordshire', 'SOUTHWEST', 'Southampton_SouthWestHampshire', 'SouthernDerbyshire', 'StHelens_Knowsley', 'Stockport', 'Suffolk', 'Sunderland', 'Tees', 'TRENT', 'Wakefield', 'Walsall', 'Warwickshire', 'WestHertfordshire', 'WestKent', 'WESTMIDLANDS', 'WestPennine', 'WestSurrey', 'WestSussex', 'WestYorkshire', 'Wigan_Bolton', 'Wiltshire', 'Wirral', 'Wolverhampton', 'Worcestershire', 'England', 'EASTERN', 'Avon', 'Barking_Havering', 'Barnet', 'Barnsley', 'Bedfordshire', 'Berkshire', 'Bexley_Greenwich', 'Birmingham', 'Bradford', 'Brent_Harrow', 'Bromley', 'Buckinghamshire', 'Bury_Rochdale', 'Calderdale_Kirklees', 'Cambridgeshire', 'Camden_Islington', 'Cornwall_IsleOfScilly', 'CountyDurham', 'Coventry', 'Croydon', 'Doncaster', 'Dorset', 'Dudley', 'Ealing,Hammersmith_Hounslow', 'East_NorthHertfordshire', 'EastKent', 'EastLancashire', 'EastLondon_TheCity', 'EastRiding', 'EastSurrey', 'EastSussex', 'Enfield_Haringey', 'Gateshead_SouthTyneside', 'Gloucestershire', 'Herefordshire', 'Hillingdon', 'IsleOfWight', 'Kensington,Chelsea_Westminster', 'Kingston_Richmond', 'Lambeth,Southwark_Lewisham', 'Leeds', 'Leicestershire', 'Lincolnshire', 'Liverpool', 'LONDON', 'Manchester', 'Merton,Sutton_Wandsworth', 'MorceambeBay', 'Newcastle_NorthTyneside', 'Norfolk', 'North_EastDevon', 'North_MidHampshire', 'NorthCheshire', 'NorthCumbria', 'NorthDerbyshire', 'NorthEssex', 'NorthNottinghamshire', 'NorthStaffordshire', 'NORTHWEST', 'NorthWestLancashire', 'NorthYorkshire', 'Northamptonshire', 'NORTHERN_YORKSHIRE', 'Northumberland', 'Nottingham', 'Oxfordshire', 'Portsmouth_SouthEastHampshire', 'Redbridge_WalthamForest', 'Rotherham', 'Salford_Trafford', 'Sandwell', 'Sefton', 'Sheffield', 'Shropshire', 'Solihull', 'Somerset', 'South_WestDevon', 'SouthCheshire', 'SouthDerbyshire', 'SOUTHEAST', 'SouthEssex', 'SouthHumber', 'SouthLancashire', 'SouthStaffordshire', 'SOUTHWEST', 'Southampton_SouthWestHampshire', 'StHelens_Knowsley', 'Stockport', 'Suffolk', 'Sunderland', 'Tees', 'TRENT', 'Wakefield', 'Walsall', 'Warwickshire', 'WestHertfordshire', 'WestKent', 'WESTMIDLANDS', 'WestPennine', 'WestSurrey', 'WestSussex', 'Wigan_Bolton', 'Wiltshire', 'Wirral', 'Wolverhampton', 'Worcestershire', 'ENGLAND', 'ANGLIA_OXFORD', 'Avon', 'Barking_Havering', 'Barnet', 'Barnsley', 'Bedfordshire', 'Berkshire', 'Bexley_Greenwich', 'Birmingham', 'Bradford', 'Brent_Harrow', 'Bromley', 'Buckinghamshire', 'Bury_Rochdale', 'Calderdale_Kirklees', 'Cambridge_Huntingdon', 'Camden_Islington', 'Cornwall_IsleOfScilly', 'CountyDurham', 'Coventry', 'Croydon', 'Doncaster', 'Dorset', 'Dudley', 'Ealing,Hammersmith_Hounslow', 'East_NorthHertfordshire', 'EastKent', 'EastLancashire', 'EastLondon_TheCity', 'EastNorfolk', 'EastRiding', 'EastSurrey', 'EastSussex', 'Enfield_Haringey', 'Gateshead_SouthTyneside', 'Gloucestershire', 'Herefordshire', 'Hillingdon', 'IsleOfWight', 'Kensington,Chelsea_Westminster', 'Kingston_Richmond', 'Lambeth,Southwark_Lewisham', 'Leeds', 'Leicestershire', 'Lincolnshire', 'Liverpool', 'Manchester', 'Merton,Sutton_Wandsworth', 'MorceambeBay', 'Newcastle_NorthTyneside', 'North_EastDevon', 'North_MidHampshire', 'NorthCheshire', 'NorthCumbria', 'NorthDerbyshire', 'NorthEssex', 'NorthNottinghamshire', 'NorthStaffordshire', 'NORTHTHAMES', 'NORTHWEST', 'NorthWestAnglia', 'NorthWestLancashire', 'NorthYorkshire', 'Northamptonshire', 'NORTHERN_YORKSHIRE', 'Northumberland', 'Nottingham', 'Oxfordshire', 'Portsmouth_SouthEastHampshire', 'Redbridge_WalthamForest', 'Rotherham', 'Salford_Trafford', 'Sandwell', 'Sefton', 'Sheffield', 'Shropshire', 'Solihull', 'Somerset', 'SOUTH_WEST', 'South_WestDevon', 'SouthCheshire', 'SouthEssex', 'SouthHumber', 'SouthLancashire', 'SouthStaffordshire', 'SOUTHTHAMES', 'Southampton_SouthWestHampshire', 'SouthernDerbyshire', 'StHelens_Knowsley', 'Stockport', 'Suffolk', 'Sunderland', 'Tees', 'TRENT', 'Wakefield', 'Walsall', 'Warwickshire', 'WestHertfordshire', 'WestKent', 'WESTMIDLANDS', 'WestPennine', 'WestSurrey', 'WestSussex', 'Wigan_Bolton', 'Wiltshire', 'Wirral', 'Wolverhampton', 'Worcestershire', 'Avon', 'Barking_Havering', 'Barnet', 'Barnsley', 'Bedfordshire', 'Berkshire', 'Bexley_Greenwich', 'Birmingham', 'Bradford', 'Brent_Harrow', 'Bromley', 'Buckinghamshire', 'Bury_Rochdale', 'Calderdale_Kirklees', 'Cambridgeshire', 'Camden_Islington', 'Cornwall_IsleOfScilly', 'CountyDurham_Darlington', 'Coventry', 'Croydon', 'Doncaster', 'Dorset', 'Dudley', 'Ealing,Hammersmith_Hounslow', 'East_NorthHertfordshire', 'EastKent', 'EastLancashire', 'EastLondon_TheCity', 'EastRiding', 'EastSurrey', 'EastSussex', 'EASTERN', 'Enfield_Haringey', 'England', 'Gateshead_SouthTyneside', 'Gloucestershire', 'Herefordshire', 'Hillingdon', 'IsleOfWight', 'Kensington,Chelsea_Westminster', 'Kingston_Richmond', 'Lambeth,Southwark_Lewisham', 'Leeds', 'Leicestershire', 'Lincolnshire', 'Liverpool', 'LONDON', 'Manchester', 'Merton,Sutton_Wandsworth', 'MorceambeBay', 'Newcastle_NorthTyneside', 'Norfolk', 'North_EastDevon', 'North_MidHampshire', 'NorthCheshire', 'NorthCumbria', 'NorthDerbyshire', 'NorthEssex', 'NorthNottinghamshire', 'NorthStaffordshire', 'NORTHWEST', 'NorthWestLancashire', 'NorthYorkshire', 'Northamptonshire', 'NORTHERN_YORKSHIRE', 'Northumberland', 'Nottingham', 'Oxfordshire', 'Portsmouth_SouthEastHampshire', 'Redbridge_WalthamForest', 'Rotherham', 'Salford_Trafford', 'Sandwell', 'Sefton', 'Sheffield', 'Shropshire', 'Solihull', 'Somerset', 'South_WestDevon', 'SouthCheshire', 'SOUTHEAST', 'SouthEssex', 'SouthHumber', 'SouthLancashire', 'SouthStaffordshire', 'SOUTHWEST', 'Southampton_SouthWestHampshire', 'SouthernDerbyshire', 'StHelens_Knowsley', 'Stockport', 'Suffolk', 'Sunderland', 'Tees', 'TRENT', 'Wakefield', 'Walsall', 'Warwickshire', 'WestHertfordshire', 'WestKent', 'WESTMIDLANDS', 'WestPennine', 'WestSurrey', 'WestSussex', 'Wigan_Bolton', 'Wiltshire', 'Wirral', 'Wolverhampton', 'Worcestershire']
+
+#for each in set(counties):
+#	print each
+geog_region = []
+geog_lookup = {}
+with open('/Users/flashton/Desktop/county_lookup') as fo:
+	for each in fo:
+		each = each.strip()
+		split_line = each.split('\t')
+		#print split_line
+		geog_lookup[split_line[0]] = split_line[1]
+
+#print geog_lookup
+
+per_year = {}
+results = {}
+
+with open('/Users/flashton/repos/nhshd_datavis/measles_data.csv') as fo:
+	for each in fo:
+		each = each.strip()
+		split_line = each.split(',', 5)
+		#print split_line
+		length = len(split_line)
+
+		geog_region = split_line[5]
+		year = split_line[0]
+		if geog_region in geog_lookup:
+			our_region = geog_lookup[geog_region]
+
+
+			if our_region in results:
+				if year in results[our_region]:
+					results[our_region][year] += 1
+				else:
+					results[our_region][year] = 1
+
+			else:
+				results[our_region] = {}
+				results[our_region][year] = 1
+
+
+
+#print per_year
+for each in per_year:
+	print each, per_year[each]
+
+#print len(set(geog_region))
+
+for each in results['EastLondon_TheCity']:
+	print each, results['EastLondon_TheCity§'][each]
+
+year_list = ['1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014']
+'''
+for y in year_list:
+	print
+	print
+	print y
+	for each in results:
+		if y in results[each]:
+			print each,
+			print results[each][y]
+
+for each in results:
+	print each
+	for every in year_list:
+		if every in results[each]:
+			print every, '\t', results[each][every]
+		else:
+			print every, '\t'
+	print
+
+
+'''
+
+

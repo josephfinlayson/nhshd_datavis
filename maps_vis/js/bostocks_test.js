1d1807867d88959b708ee5978d7d53819c583b8b
joseph finlayson <joseph.finlayson@gmail.com>
2014-05-25 13:14:10 +0100

    more munging

--- maps_vis/js/bostocks_test.js
+++ maps_vis/js/bostocks_test.js
@@ -8,26 +8,116 @@ jQuery(document).ready(function($) {
         .parallels([50, 60])
         .scale(6000)
         .translate([width / 2, height / 2])
+    vac_rates = [];
+    var n = 0;
 
-    var data_redux = d3.tsv("data_redux", function(error, d) {
-        console.log(d);
+    data_redux = d3.tsv("data_redux", function(d) {
+        // console.log(d);
         return {
-            d.00_01_mmr: "94",
-            d.00_01_number_children: "4917",
-            d.d.97_98_mmr: "92",
-            d.97_98_number_children: "4746",
-            d.98_99_mmr: "95",
-            d.98_99_number_children: "4876",
-            d.99_00_mmr: "94",
-            d.99_00_number_children: "4899",
-            d.geog_region: "Barking_Havering",
-            d.mapped_to_map: "Barking and Dagenham",
-        }
-        //     data.forEach(function(d) {
-        //         d.geog_region = d.geog_region;
-        //         d[97_98_number_children] += d[97_98_number_children];
-        //     });
-    });
+            mapped_to_map: d.mapped_to_map, //   "Barking and Dagenham"
+            time: d.time, //"Barking_Havering"
+            y1998_mmr: d.y1998_mmr, //   "92"
+            y1998_num: d.y1998_num, //   "4746"
+            y1999_mmr: d.y1999_mmr, //   "95"
+            y1999_num: d.y1999_num, //   "4876"
+            y2000_mmr: d.y1998_mmr, //   "94"
+            y2000_num: d.y2000_num, //   "4899"
+            y2001_mmr: d.y2001_mmr, //   "94"
+            y2001_num: d.y2001_num //  "4917"
+        };
+    }, function(err, rows) {
+        console.log(rows);
+        vac_rates = rows;
+        d3.json("maps_topo.json", function(error, uk) {
+            var filtered = [];
+            to_filter = svg.selectAll(".subunit").data(topojson.feature(uk, uk.objects.maps).features);
+            this.uk = uk;
+            svg.selectAll(".subunit")
+
+            to_filter
+            // filter(function(index) {
+            //     console.log(index)
+            // })
+            // // .data(topojson.feature(uk, uk.objects.maps).features)
+            .enter().append('path')
+                .filter(function(d) {
+                    // console.log(d);
+                    if (d.properties.NAME_1 === "England") {
+                        return true;
+                    }
+                }).attr('data-deaths', function(d) {
+                    rows.forEach(function(row) {
+                        if (d.properties.NAME_2 == row.mapped_to_map) {
+                            // console.log("asdsa");
+                            return row.y2000_mmr;
+                        }
+                    })
+                })
+                .attr("class", function(d) {
+                    return "subunit " + d.id;
+                }).text(function(d) {
+                    // console.log(d);
+                }).attr('data-county', function(d) {
+                    // console.log(d.properties.NAME_2)
+                    return d.properties.NAME_2
+
+                })
+                .attr("d", path)
+                .style('fill', function(d) {
+                    if (d.properties.NAME_1 !== "England") {
+                        return d3.rgb(255, 255, 255);
+                    }
+                    return d3.rgb(rand(), rand(), rand())
+                }).style('path', function(d) {
+                    if (d.properties.NAME_1 !== "England") {
+                        return d3.rgb(255, 255, 255);
+                    }
+                    return d3.rgb(rand(), rand(), rand())
+                })
+                .on("mouseover", function(d) {
+                    console.log(d.properties.NAME_2);
+                    $('#county').html(d.properties.NAME_2)
+                })
+                .on("mouseout", function(d) {
+                    console.log("path mouseout");
+                })
+                .on("mousemove", function(d) {
+                    console.log("path mouseout");
+                })
+                .on("touchmove", function(d) {
+                    console.log("path mouseout");
+                })
+                .on("click", function(d) {
+                    console.log("path click");
+                });
+        });
+    })
+
+    // data_redux = d3.tsv.parse(data_redux);
+
+    // function(error, d) {
+    //     // console.log
+
+    // });
+
+    //         // // data.forEach(function(d) {
+    //         //         d.geog_region = d.geog_region;
+    //         //         d[97_98_number_children] += d[97_98_number_children];
+    //         //     });
+    //     },
+    //     function(error, rows) {
+    //         console.log(rows);
+    //         console.log(error);
+    //         console.log("error");
+
+
+    //         console.log("rows");
+
+    //         vac_rates[n] = rows
+    //         n++
+
+    //     }
+    // );
 
     var path =
         d3.geo.path()
@@ -40,66 +130,7 @@ jQuery(document).ready(function($) {
     rand = function() {
         return Math.floor(Math.random() * 100);
     }
-    d3.json("maps_topo.json", function(error, uk) {
-        var filtered = [];
-        to_filter = svg.selectAll(".subunit").data(topojson.feature(uk, uk.objects.maps).features);
-        this.uk = uk;
-        svg.selectAll(".subunit")
 
-        to_filter
-        // filter(function(index) {
-        //     console.log(index)
-        // })
-        // // .data(topojson.feature(uk, uk.objects.maps).features)
-        .enter().append('path')
-            .filter(function(d) {
-                console.log(d);
-                if (d.properties.NAME_1 === "England") {
-                    return true;
-                }
-            }).attr('data-deaths', function(d) {
-                // console.log(d.properties.NAME_2)
-                console.log(data_redux);
-
-            })
-            .attr("class", function(d) {
-                return "subunit " + d.id;
-            }).text(function(d) {
-                // console.log(d);
-            }).attr('data-county', function(d) {
-                // console.log(d.properties.NAME_2)
-                return d.properties.NAME_2
-
-            })
-            .attr("d", path)
-            .style('fill', function(d) {
-                if (d.properties.NAME_1 !== "England") {
-                    return d3.rgb(255, 255, 255);
-                }
-                return d3.rgb(rand(), rand(), rand())
-            }).style('path', function(d) {
-                if (d.properties.NAME_1 !== "England") {
-                    return d3.rgb(255, 255, 255);
-                }
-                return d3.rgb(rand(), rand(), rand())
-            })
-            .on("mouseover", function(d) {
-                console.log(d.properties.NAME_2);
-                $('#county').html(d.properties.NAME_2)
-            })
-            .on("mouseout", function(d) {
-                console.log("path mouseout");
-            })
-            .on("mousemove", function(d) {
-                console.log("path mouseout");
-            })
-            .on("touchmove", function(d) {
-                console.log("path mouseout");
-            })
-            .on("click", function(d) {
-                console.log("path click");
-            });
-    });
 
 
 
